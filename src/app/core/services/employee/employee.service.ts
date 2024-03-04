b08f83b3b91c30903982ad0b28cd60d8deae31a7
Jessé Antônio <jesseantonio1321@gmail.com>
2024-03-04 01:22:59 -0300

    list page

--- src/app/core/services/employee/employee.service.ts
+++ src/app/core/services/employee/employee.service.ts
@@ -13,7 +13,7 @@ export class EmployeeService {
 
 
   get() {
-    return this.http.get(`${this.baseUrl}/employee`);
+    return this.http.get(`${this.baseUrl}/employees`);
   }
 
   getById(id: string) {
